fbaf00f8406b8c0722869927fe4f8c9731455828
Junior Paula <slucivaldo509@gmail.com>
2021-11-23 21:54:42 -0300

    created card models

--- src/models/Carro.js
+++ src/models/Carro.js
@@ -3,12 +3,45 @@ import Sequelize, { Model } from 'sequelize';
 export default class Carro extends Model {
   static init(sequelize) {
     super.init({
-      marca: Sequelize.STRING,
-      modelo: Sequelize.STRING,
-      ano: Sequelize.STRING,
-      preco: Sequelize.FLOAT,
+      marca: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'Informe a marca do veículo!',
+          },
+        },
+      },
+      modelo: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'Informe a modelo do veículo!',
+          },
+        },
+      },
+      ano: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'Informe o ano do veículo!',
+          },
+        },
+      },
+      preco: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Preço precisa ser um número de ponto flutuante ex: 13000.00',
+          },
+        },
+      },
     }, {
       sequelize,
     });
+    return this;
   }
 }
