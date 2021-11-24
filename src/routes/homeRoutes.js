84bda94d812879ec4ac4e05f60d00849e7b6506c
Junior Paula <slucivaldo509@gmail.com>
2021-11-23 21:54:02 -0300

    created routes of the CRUD operation

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -7,5 +7,9 @@ const router = new Router();
 
 /** rotas da home */
 router.get('/', homeController.index);
+router.post('/', homeController.create);
+router.get('/:id', homeController.show);
+router.put('/:id', homeController.update);
+router.delete('/:id', homeController.delete);
 
 export default router;
